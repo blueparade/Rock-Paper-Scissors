91480ae83cc1b0684533d450245f2183aa00383a
blueparade <blueistyping@gmail.com>
2026-03-21 16:10:23 +0700

    Move if-else conditionals to getComputerChoice function

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 function getComputerChoice () {
-    return Math.floor(Math.random() * 3 + 1);
-   
+    let result = Math.floor(Math.random() * 3 + 1);
+
+   if (result === 1) {
+    return("Rock");
+    } else if (result === 2) {
+    return("Paper");
+    } else if (result === 3) {
+    return("Scissors");
+    }
 }
 
-if (getComputerChoice () === 1) {
-        console.log("Rock");
-    } else if (getComputerChoice () === 2) {
-        console.log("Paper");
-    } else {
-        console.log("Scissors");
+console.log(getComputerChoice())
+
+
+function getHumanChoice () {
+    sign = window.prompt("Insert Choice");
+}
+
+if (sign === "Rock") {
+    console.log(1);
+} else if (sign === "Paper") {
+    console.log(2);
     }
