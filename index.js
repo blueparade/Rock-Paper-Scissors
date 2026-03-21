f76cb4b1215d6f4d2fc509409e4b5c3d2911f93f
blueparade <blueistyping@gmail.com>
2026-03-21 16:28:46 +0700

    Add getHumanChoice function
    this function takes the user choice and returns it

--- index.js
+++ index.js
@@ -2,23 +2,26 @@ function getComputerChoice () {
     let result = Math.floor(Math.random() * 3 + 1);
 
    if (result === 1) {
-    return("Rock");
+        return("Rock");
     } else if (result === 2) {
-    return("Paper");
+        return("Paper");
     } else if (result === 3) {
-    return("Scissors");
+        return("Scissors");
     }
 }
 
-console.log(getComputerChoice())
+
 
 
 function getHumanChoice () {
-    sign = window.prompt("Insert Choice");
+    let sign = window.prompt("Insert Choice");
+
+    if (sign === "Rock") {
+        return("Rock");
+    } else if (sign === "Paper") {
+        return("Paper");
+    } else if (sign === "Scissors") {
+        return("Scissors");
+    }
 }
 
-if (sign === "Rock") {
-    console.log(1);
-} else if (sign === "Paper") {
-    console.log(2);
-    }
